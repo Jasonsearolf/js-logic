480250612c30b5b774e4b5e68b9856fbcbc040a9
jason <jason@adatier.com>
2024-06-07 19:01:05 +0000

    task 46 done

---
+++ 01-arrays/task46_substring.js
@@ -0,0 +1,25 @@
+
+
+const substrings = (palabra) => {
+
+    let substrings = [];
+
+    for(let letra in palabra){
+        
+        let comienzo = parseInt(letra);
+
+        console.log(comienzo);
+
+        for (let i = 0; i <= palabra.length - comienzo; i++) {
+            
+            let final = parseInt(i) + parseInt(comienzo);
+
+            substrings.push(palabra.substring(comienzo, final));
+            
+        }
+    }
+
+    return substrings.filter(palabra => palabra.length >= 1);
+}
+
+console.log(substrings("hola"));
